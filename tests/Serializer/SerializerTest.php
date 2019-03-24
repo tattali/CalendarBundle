ba47481dee52e68f80d1a087b5b6a3e73be0f822
Théo Attali <theoattali@gmail.com>
2019-03-24 22:05:58 +0100

    Serializer tests with phpunit

---
+++ tests/Serializer/SerializerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace CalendarBundle\Tests\Serializer;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use CalendarBundle\Entity\Event;
+use CalendarBundle\Serializer\Serializer;
+use PHPUnit\Framework\TestCase;
+
+class SerializerTest extends TestCase
+{
+    public function setUp(): void
+    {
+        $this->eventEntity1 = $this->createMock(Event::class);
+        $this->eventEntity2 = $this->createMock(Event::class);
+
+        $this->serializer = new Serializer();
+    }
+
+    public function testItSerializesDataSuccessfully()
+    {
+        $this->eventEntity1->method('toArray')->willReturn(
+            [
+                'title' => 'Event 1',
+                'start' => '2015-01-20T11:50:00Z',
+                'allDay' => false,
+                'end' => '2015-01-21T11:50:00Z',
+            ]
+        );
+        $this->eventEntity2->method('toArray')->willReturn(
+            [
+                'title' => 'Event 2',
+                'start' => '2015-01-22T11:50:00Z',
+                'allDay' => true,
+            ]
+        );
+
+        $data = json_encode([
+            [
+                'title' => 'Event 1',
+                'start' => '2015-01-20T11:50:00Z',
+                'allDay' => false,
+                'end' => '2015-01-21T11:50:00Z',
+            ],
+            [
+                'title' => 'Event 2',
+                'start' => '2015-01-22T11:50:00Z',
+                'allDay' => true,
+            ]
+        ]);
+
+        $this->assertEquals($data, $this->serializer->serialize([$this->eventEntity1, $this->eventEntity2]));
+    }
+
+    public function testSerializesShouldReturnEmtpyIfEventsAreEmpty()
+    {
+        $this->assertEquals('[]', $this->serializer->serialize([]));
+    }
+}
