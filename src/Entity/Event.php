4e9c83e599400da5c719c8c8048f168222aef6a7
treeindark <treeindark@gmail.com>
2023-05-05 15:04:37 +0200

    Update Event.php
    Event with allday don't display in allDay slot
    
    change date format for start event to 'Y-m-d' if allDay is true.
    
    rapid fix for FullCalendar Standard Bundle v6.1.6

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -123,6 +123,10 @@ class Event
             $event['resourceId'] = $this->getResourceId();
         }
 
-        return array_merge($event, $this->getOptions());
+         $toReturn = array_merge($event, $this->getOptions());
+
+        if ($toReturn['allDay'])
+            $toReturn['start'] =  $this->getStart()->format('Y-m-d');
+        return $toReturn;
     }
 }
