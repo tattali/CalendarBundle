4ca920cba121bc1a039fa17629219c330a238fb5
Christopher Georg <christopher.georg@sr-travel.de>
2024-06-10 14:22:01 +0200

    feat: fix deprecation for Extension

--- src/DependencyInjection/CalendarExtension.php
+++ src/DependencyInjection/CalendarExtension.php
@@ -6,8 +6,8 @@ namespace CalendarBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
